3574b5eb3907b88a157d2a664dd5b5d4098550e0
Abayomi igwubor <igwubor@outlook.com>
2019-08-19 12:12:38 +0100

    ranGulp/addedTele

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,16 @@
 var gulp = require('gulp');
 var cssnano = require('gulp-cssnano');
 var concat = require('gulp-concat');
- 
+
 gulp.task('scripts', function() {
-  return gulp.src('./js/*.js')
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('./mainJs/'));
+	return gulp.src('./js/*.js').pipe(concat('main.js')).pipe(gulp.dest('./mainJs/'));
 });
 gulp.task('cssnano', function() {
-    return gulp.src('./style/*.css')
-        .pipe(cssnano())
-        .pipe(gulp.dest('./mainCss'));
+	return gulp.src('./style/*.css').pipe(cssnano()).pipe(gulp.dest('./mainCss'));
 });
-gulp.task('run', ['scripts','cssnano'])
-gulp.task('watch', ()=>{
-   gulp.watch('./js/*.js', ['scripts'])
-   gulp.watch('./style/*.css', ['cssnano'])
-})
-gulp.task('default', ['run', 'watch'])
-
-
- 
+gulp.task('run', [ 'scripts', 'cssnano' ]);
+gulp.task('watch', () => {
+	gulp.watch('./js/*.js', [ 'scripts' ]);
+	gulp.watch('./style/*.css', [ 'cssnano' ]);
+});
+gulp.task('default', [ 'run', 'watch' ]);
